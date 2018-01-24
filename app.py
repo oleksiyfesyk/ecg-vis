6248e1b807f06c1f64be35307031936982a742bc
Meowth Pokemon <oleksiyfesyk@gmail.com>
2018-01-24 22:06:37 +0200

    Upload example

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
 
+# From upload example
+import dash_table_experiments as dt
+import pandas as pd
+import io
+
 
 # Setup the app
 # Make sure not to change this file name or the variable names below,
@@ -21,6 +26,26 @@ app = dash.Dash(__name__, server=server)
 
 
 # Put your Dash code here
+
+app.layout = html.Div([
+    html.H1('Dash'),
+    dcc.Upload(id='upload'),
+    dt.DataTable(
+        id='datatable',
+        rows=[{}]
+    ),
+], className="container")
+
+@app.callback(
+    Output('datatable', 'rows'),
+    [Input('upload', 'contents')])
+def update_figure(content):
+    if not content:
+        return []
+    dff = pd.read_csv(io.StringIO(content))
+    return dff.to_dict('records')
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 
 # Run the Dash app
