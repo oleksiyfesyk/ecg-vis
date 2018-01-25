93ec3c2ec7bffd3915708142cf563ebbd11bd26d
Hanna Zhelezniak <newgreen.sapphire@gmail.com>
2018-01-25 15:38:41 +0200

    Auth first try

--- app.py
+++ app.py
@@ -2,11 +2,105 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_auth
 from pandas_datareader import data as web
 from datetime import datetime as dt
 import flask
 import time
 import os
+import plotly
+
+
+
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['iurii', 'iurii']
+]
+
+
+server = flask.Flask('app')
+server.secret_key = os.environ.get('secret_key', 'secret')
+
+#app = dash.Dash('app', server=server)
+
+app = dash.Dash('auth', server=server)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+app.scripts.config.serve_locally = False
+dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+
+
+
+
+
+app.layout = html.Div([
+	html.H1('Welcome to the app'),
+    html.H3('You are successfully authorized'),
+    html.H1('Stock Tickers'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Samsung', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+], className="container")
+
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value, data_source='google',
+        start=dt(2017, 1, 1), end=dt.now())
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close,
+            'line': {
+                'width': 3,
+                'shape': 'spline'
+            }
+        }],
+        'layout': {
+            'margin': {
+                'l': 30,
+                'r': 20,
+                'b': 30,
+                't': 20
+            }
+        }
+    }
+
+
+
+app.scripts.config.serve_locally = True
+app.css.append_css({
+    'external_url': (
+	'https://cdn.rawgit.com/plotly/dash-app-stylesheets/96e31642502632e86727652cf0ed65160a57497f/dash-hello-world.css'
+    )
+})
+
+
+if 'DYNO' in os.environ:
+    app.scripts.append_script({
+        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+    })
+
+
+if __name__ == '__main__':
+    app.run_server()
+    
+
+
+
+
+
+
 
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
