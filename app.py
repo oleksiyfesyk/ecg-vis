00481bf8cd122a3e497fadc6d10e273b14b4433a
Meowth Pokemon <oleksiyfesyk@gmail.com>
2018-01-25 20:12:37 +0200

    add _tkinter import statement

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import plotly
 import wfdb
 import plotly.tools as tls
 import tkinter
+import _tkinter
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = [
