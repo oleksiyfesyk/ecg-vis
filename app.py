c07db50bf37ef06ae1573b4e5a579776edfc0fd7
Meowth Pokemon <oleksiyfesyk@gmail.com>
2018-01-25 14:15:20 +0200

    Change app to Hello World Dash app

--- app.py
+++ app.py
@@ -2,33 +2,69 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
-import pandas as pd
-import io
+from pandas_datareader import data as web
+from datetime import datetime as dt
+import flask
+import time
+import os
 
-app = dash.Dash()
+server = flask.Flask('app')
+server.secret_key = os.environ.get('secret_key', 'secret')
+
+app = dash.Dash('app', server=server)
+
+app.scripts.config.serve_locally = False
+dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
 
 app.layout = html.Div([
-    html.H1('Dash'),
-    dcc.Upload(id='upload'),
-    dt.DataTable(
-        id='datatable',
-        rows=[{}]
+    html.H1('Stock Tickers'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
     ),
+    dcc.Graph(id='my-graph')
 ], className="container")
 
-@app.callback(
-    Output('datatable', 'rows'),
-    [Input('upload', 'contents')])
-def update_figure(content):
-    if not content:
-        return []
-    dff = pd.read_csv(io.StringIO(content))
-    return dff.to_dict('records')
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value, data_source='google',
+        start=dt(2017, 1, 1), end=dt.now())
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close,
+            'line': {
+                'width': 3,
+                'shape': 'spline'
+            }
+        }],
+        'layout': {
+            'margin': {
+                'l': 30,
+                'r': 20,
+                'b': 30,
+                't': 20
+            }
+        }
+    }
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+app.css.append_css({
+    'external_url': (
+	'https://cdn.rawgit.com/plotly/dash-app-stylesheets/96e31642502632e86727652cf0ed65160a57497f/dash-hello-world.css'
+    )
+})
+
+if 'DYNO' in os.environ:
+    app.scripts.append_script({
+        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+    })
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
+    app.run_server()
