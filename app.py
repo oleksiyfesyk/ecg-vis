33b29cd91dbccd30b535c46dda2563104d390db4
Meowth Pokemon <oleksiyfesyk@gmail.com>
2018-01-25 21:13:50 +0200

    remove validation ;(

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_auth
+#import dash_auth
 #from pandas_datareader import data as web
 #from datetime import datetime as dt
 import flask
@@ -22,18 +22,18 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 
 
 # Keep this out of source code repository - save in a file or a database
-VALID_USERNAME_PASSWORD_PAIRS = [
-    ['iurii', 'iurii']
-]
+#VALID_USERNAME_PASSWORD_PAIRS = [
+#    ['iurii', 'iurii']
+#]
 
 
-#app = dash.Dash('app', server=server)
+app = dash.Dash('app', server=server)
 
-app = dash.Dash('auth', server=server)
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
+#app = dash.Dash('auth', server=server)
+#auth = dash_auth.BasicAuth(
+#    app,
+#    VALID_USERNAME_PASSWORD_PAIRS
+#)
 
 app.scripts.config.serve_locally = False
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
