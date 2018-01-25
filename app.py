c9bfcbf2ef9e0385caa13b40085eee677bb1a36f
Meowth Pokemon <oleksiyfesyk@gmail.com>
2018-01-25 11:14:43 +0200

    Change to simple upload example, trying to use PythonAnywhere

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-# Import required libraries
-import os
-from random import randint
-
-import plotly.plotly as py
-from plotly.graph_objs import *
-
-import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-
-# From upload example
 import dash_table_experiments as dt
 import pandas as pd
 import io
 
-
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
-
-
-# Put your Dash code here
+app = dash.Dash()
 
 app.layout = html.Div([
     html.H1('Dash'),
@@ -48,6 +29,6 @@ def update_figure(content):
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 
-# Run the Dash app
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    app.run_server(debug=True)
+
