6e6d448d73ab2f18158fd06742ae825e857c03cb
Hanna Zhelezniak <newgreen.sapphire@gmail.com>
2018-01-26 14:03:34 +0200

    change file app.py(add auth) and req.txt(modify dash and dash_auth) (change branch to master)

--- app.py
+++ app.py
@@ -2,41 +2,44 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_auth
+#import dash_auth
 #from pandas_datareader import data as web
 #from datetime import datetime as dt
 import flask
 #import time
 import matplotlib
-matplotlib.use('Qt5Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import plotly
 import wfdb
 import plotly.tools as tls
 
-VALID_USERNAME_PASSWORD_PAIRS = [
-    ['iurii', 'iurii'],
-	['alex','alex'],
-	['anna','anna']
-]
 
 
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
 
-#app = dash.Dash('app', server=server)
+
+# Keep this out of source code repository - save in a file or a database
+#VALID_USERNAME_PASSWORD_PAIRS = [
+#    ['iurii', 'iurii']
+#]
 
 
-app = dash.Dash('auth', server=server)
+app = dash.Dash('app', server=server)
 
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
+#app = dash.Dash('auth', server=server)
+#auth = dash_auth.BasicAuth(
+#    app,
+#    VALID_USERNAME_PASSWORD_PAIRS
+#)
 
 app.scripts.config.serve_locally = False
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+
+
+
 
 
 app.layout = html.Div([
@@ -46,9 +49,9 @@ app.layout = html.Div([
     dcc.Dropdown(
         id='my-dropdown',
         options=[
-            {'label': 'Subject 3a', 'value': 'aami3a'},
-            {'label': 'Subject 3b', 'value': 'aami3b'},
-            {'label': 'Subject 3c', 'value': 'aami3c'}
+            {'label': 'Subject 100', 'value': 'aami3a'}
+            {'label': 'Subject 101', 'value': 'aami3b'},
+            {'label': 'Subject 102', 'value': 'aami3c'},
 #			{'label': 'Subject 103', 'value': '103'},
 #			{'label': 'Subject 104', 'value': '104'}
         ],
